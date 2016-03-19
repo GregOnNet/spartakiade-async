11e59386c74f1db085a3733e0256674de5c91516
Gregor Woiwode <gw@co-it.eu>
2016-03-19 12:46:51 +0100

    Adds test testing cpu bound operation

---
+++ src/ConsoleApplication/AsyncPlayground.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+  [TestFixture]
+  class AsyncPlayground
+  {
+    [Test]
+    public async Task TaskRun()
+    {
+      await Task.Run(() => DoCpuBoundOperationAsync(1000));
+    }
+
+    public async Task DoCpuBoundOperationAsync(int delayInMilliSeconds)
+    {
+      await Task.Delay(delayInMilliSeconds);
+    }
+  }
+}

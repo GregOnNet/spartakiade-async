c1304483d3e3aaa3677a402c9b697405152d8afb
Gregor Woiwode <gw@co-it.eu>
2016-03-19 13:04:15 +0100

    Adds sample using Parallel.For

--- src/ConsoleApplication/AsyncPlayground.cs
+++ src/ConsoleApplication/AsyncPlayground.cs
@@ -1,20 +1,56 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
   [TestFixture]
-  class AsyncPlayground
+  public class TaskRunVsTaskFactorynew
   {
     [Test]
     public async Task TaskRun()
     {
-      await Task.Run(() => DoCpuBoundOperationAsync(1000));
+      Console.Write("Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+      await Task.Run(() => DoCpuBoundOperationAsync(200));
+    }
+
+    [Test]
+    public async Task TaskFactoryStartNew()
+    {
+      Console.Write("Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+      await Task.Factory.StartNew(() => DoCpuBoundOperationAsync(200));
     }
 
     public async Task DoCpuBoundOperationAsync(int delayInMilliSeconds)
     {
       await Task.Delay(delayInMilliSeconds);
+    }
+  }
+
+  [TestFixture]
+  public class ParallelFor
+  {
+    [Test]
+    public void CalculateFibonacciSecqunce()
+    {
+      Parallel.For(1, 10, (i,state) => Console.WriteLine(
+        Fibonacci(i)
+      ));
+    }
+
+    public int Fibonacci(int n)
+    {
+      int a = 0;
+      int b = 1;
+      
+      for (int i = 0; i < n; i++)
+      {
+        int temp = a;
+        a = b;
+        b = temp + b;
+      }
+      return a;
     }
   }
 }
